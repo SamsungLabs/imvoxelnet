1369332328ea1fd0553d8a3a12be388be71beb29
Danila Rukhovich <danrukh@gmail.com>
2020-12-28 11:10:18 +0300

    try resnet18

---
+++ configs/detr3d/detr3d_sunrgbd-3d-10class_copy-4.py
@@ -0,0 +1,136 @@
+model = dict(
+    type='FCOS3D',
+    pretrained='torchvision://resnet50',
+    backbone=dict(
+        type='ResNet',
+        depth=18,
+        num_stages=4,
+        out_indices=(3, ),
+        frozen_stages=1,
+        norm_cfg=dict(type='BN', requires_grad=False),
+        norm_eval=True,
+        style='pytorch'),
+    bbox_head=dict(
+        type='Transformer3DHead',
+        num_classes=10,
+        in_channels=512,
+        num_fcs=2,
+        transformer=dict(
+            type='Transformer',
+            embed_dims=256,
+            num_heads=8,
+            num_encoder_layers=0,  # TODO: 6 ?
+            num_decoder_layers=3,
+            feedforward_channels=512,
+            dropout=0.1,
+            act_cfg=dict(type='ReLU', inplace=True),
+            norm_cfg=dict(type='LN'),
+            num_fcs=2,
+            pre_norm=False,
+            return_intermediate_dec=True),
+        positional_encoding=dict(
+            type='SinePositionalEncoding', num_feats=128, normalize=True)))
+# training and testing settings
+train_cfg = dict(
+    assigner=dict(
+        type='HungarianAssigner3D', cls_weight=1., bbox_weight=5., iou_weight=2.,
+        iou_calculator=dict(type='BboxOverlaps3D', coordinate='depth'), iou_mode='giou'))
+test_cfg = dict(max_per_img=32)
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+dataset_type = 'SUNRGBD2DDataset'
+data_root = 'data/sunrgbd/'
+class_names = ('cabinet', 'bed', 'chair', 'sofa', 'table', 'desk', 'dresser',
+               'night_stand', 'sink', 'lamp')
+# train_pipeline, NOTE the img_scale and the Pad's size_divisor is different
+# from the default setting in mmdet.
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations3D'),
+    dict(type='Resize', img_scale=[(288, 384), (672, 896)], keep_ratio=True),
+    dict(type='RandomFlip2D', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=1),
+    dict(type='DefaultFormatBundle3D', class_names=class_names),
+    dict(type='Collect3D', keys=['img', 'gt_bboxes_3d', 'gt_labels_3d'])
+]
+# test_pipeline, NOTE the Pad's size_divisor is different from the default
+# setting (size_divisor=32). While there is little effect on the performance
+# whether we use the default setting or use size_divisor=1.
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(640, 480),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip2D'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=1),
+            dict(
+                type='DefaultFormatBundle3D',
+                class_names=class_names,
+                with_label=False),
+            dict(type='Collect3D', keys=['img'])
+        ])
+]
+data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=4,
+    train=dict(
+        type='RepeatDataset',
+        times=3,
+        dataset=dict(
+            type=dataset_type,
+            data_root=data_root,
+            ann_file=data_root + 'sunrgbd_infos_train.pkl',
+            pipeline=train_pipeline,
+            classes=class_names,
+            filter_empty_gt=True,
+            # we use box_type_3d='LiDAR' in kitti and nuscenes dataset
+            # and box_type_3d='Depth' in sunrgbd and scannet dataset.
+            box_type_3d='Depth')),
+    val=dict(
+        type=dataset_type,
+        data_root=data_root,
+        ann_file=data_root + 'sunrgbd_infos_val.pkl',
+        pipeline=test_pipeline,
+        classes=class_names,
+        test_mode=True,
+        box_type_3d='Depth'),
+    test=dict(
+        type=dataset_type,
+        data_root=data_root,
+        ann_file=data_root + 'sunrgbd_infos_val.pkl',
+        pipeline=test_pipeline,
+        classes=class_names,
+        test_mode=True,
+        box_type_3d='Depth'))
+# optimizer
+optimizer = dict(
+    type='AdamW',
+    lr=0.0001,
+    weight_decay=0.0001,
+    paramwise_cfg=dict(
+        custom_keys={'backbone': dict(lr_mult=0.1, decay_mult=1.0)}))
+optimizer_config = dict(grad_clip=dict(max_norm=0.1, norm_type=2))
+# learning policy
+lr_config = dict(policy='step', step=[33])
+total_epochs = 50
+
+checkpoint_config = dict(interval=1, max_keep_ckpts=1)
+# yapf:disable
+log_config = dict(
+    interval=50,
+    hooks=[
+        dict(type='TextLoggerHook'),
+        dict(type='TensorboardLoggerHook')
+    ])
+# yapf:enable
+evaluation = dict(interval=1)
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+load_from = None
+resume_from = None
+workflow = [('train', 1)]
