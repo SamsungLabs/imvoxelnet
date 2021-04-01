bb4946f4e3f512bc06bbcdd279e61942bbc6fa37
Danila Rukhovich <danrukh@gmail.com>
2021-04-01 13:42:59 +0300

    add scannet head; refactor configs

--- mmdet3d/models/detectors/atlas.py
+++ mmdet3d/models/detectors/atlas.py
@@ -9,9 +9,11 @@ from mmdet3d.core import bbox3d2result
 class AtlasDetector(BaseDetector):
     def __init__(self,
                  backbone,
-                 neck=None,
-                 neck_3d=None,
-                 bbox_head=None,
+                 neck,
+                 neck_3d,
+                 bbox_head,
+                 n_voxels,
+                 voxel_size,
                  train_cfg=None,
                  test_cfg=None,
                  pretrained=None):
@@ -22,6 +24,9 @@ class AtlasDetector(BaseDetector):
         bbox_head.update(train_cfg=train_cfg)
         bbox_head.update(test_cfg=test_cfg)
         self.bbox_head = build_head(bbox_head)
+        self.bbox_head.voxel_size = voxel_size
+        self.n_voxels = n_voxels
+        self.voxel_size = voxel_size
         self.train_cfg = train_cfg
         self.test_cfg = test_cfg
         self.init_weights(pretrained=pretrained)
@@ -33,85 +38,46 @@ class AtlasDetector(BaseDetector):
         self.neck_3d.init_weights()
         self.bbox_head.init_weights()
 
-    def forward_train(self, img, img_metas, gt_bboxes_3d, gt_labels_3d, **kwargs):
-        x = self.extract_feat(img)
-
-        voxel_size = torch.tensor(self.train_cfg['voxel_size']).float()
-        volume, valid = [], []
-        for feature, img_meta in zip(x, img_metas):
-            projection = self._compute_projection(img_meta)
-            origin = torch.tensor(img_meta['lidar2img']['origin'])
-            new_origin = get_origin(
-                n_voxels=torch.tensor(self.train_cfg['n_voxels']),
-                voxel_size=voxel_size,
-                origin=origin
-            )
-            img_volume, img_valid = backproject(
-                voxel_dim=self.train_cfg['n_voxels'],
-                voxel_size=voxel_size.reshape(1, 3, 1).to(x.device),
-                origin=new_origin.reshape(1, 3),
-                projection=projection.to(x.device),
-                features=feature
-            )
-            volume.append(img_volume)
-            valid.append(img_valid)
-        volume = torch.stack(volume)
-        valid = torch.stack(valid)
-        volume = volume.sum(dim=1)
-        valid = valid.sum(dim=1)
-        x = volume / valid
-        x = x.transpose(0, 1)
-        valid = valid > 0
-        x[:, ~valid[:, 0]] = .0
-        x = x.transpose(0, 1)
-
-        x = self.neck_3d(x)
-        losses = self.bbox_head.forward_train(x, valid.float(), img_metas, gt_bboxes_3d, gt_labels_3d)
-        return losses
-
-    def extract_feat(self, img):
+    def extract_feat(self, img, img_metas):
         batch_size = img.shape[0]
         img = img.reshape([-1] + list(img.shape)[2:])
         x = self.backbone(img)
         x = self.neck(x)[0]
-        return x.reshape([batch_size, -1] + list(x.shape[1:]))
+        x = x.reshape([batch_size, -1] + list(x.shape[1:]))
+
+        volumes, valids = [], []
+        for feature, img_meta in zip(x, img_metas):
+            projection = self._compute_projection(img_meta).to(x.device)
+            points = get_points(
+                n_voxels=torch.tensor(self.n_voxels),
+                voxel_size=torch.tensor(self.voxel_size),
+                origin=torch.tensor(img_meta['lidar2img']['origin'])
+            ).to(x.device)
+            volume, valid = backproject(feature, points, projection)
+            volume = volume.sum(dim=0)
+            valid = valid.sum(dim=0)
+            volume = volume / valid
+            valid = valid > 0
+            volume[:, ~valid[0]] = .0
+            volumes.append(volume)
+            valids.append(valid)
+        x = torch.stack(volumes)
+        valids = torch.stack(valids)
+        x = self.neck_3d(x)
+        return x, valids
+
+
+    def forward_train(self, img, img_metas, gt_bboxes_3d, gt_labels_3d, **kwargs):
+        x, valids = self.extract_feat(img, img_metas)
+        losses = self.bbox_head.forward_train(x, valids.float(), img_metas, gt_bboxes_3d, gt_labels_3d)
+        return losses
 
     def forward_test(self, img, img_metas, **kwargs):
         # not supporting aug_test for now
         return self.simple_test(img, img_metas)
 
     def simple_test(self, img, img_metas):
-        x = self.extract_feat(img)
-
-        ys, valids = [], []
-        voxel_size = torch.tensor(self.test_cfg['voxel_size']).float()
-        for features, img_meta in zip(x, img_metas):
-            projections = self._compute_projection(img_meta)
-            origin = torch.tensor(img_meta['lidar2img']['origin'])
-            volume, valid = .0, 0
-            for feature, projection in zip(features, projections):
-                new_origin = get_origin(
-                    n_voxels=torch.tensor(self.test_cfg['n_voxels']),
-                    voxel_size=voxel_size,
-                    origin=origin
-                )
-                img_volume, img_valid = backproject(
-                    voxel_dim=self.test_cfg['n_voxels'],
-                    voxel_size=voxel_size.reshape(1, 3, 1).to(x.device),
-                    origin=new_origin.reshape(1, 3),
-                    projection=projection[None, ...].to(x.device),
-                    features=feature[None, ...]
-                )
-                volume += img_volume[0]
-                valid += img_valid[0]
-            y = volume / valid
-            y[:, valid[0] == 0] = .0
-            ys.append(y)
-            valids.append(valid > 0)
-        x = torch.stack(ys)
-        valids = torch.stack(valids)
-
-        x = self.neck_3d(x)
+        x, valids = self.extract_feat(img, img_metas)
         x = self.bbox_head(x)
         bbox_list = self.bbox_head.get_bboxes(*x, valids.float(), img_metas)
         bbox_results = [
@@ -119,7 +85,6 @@ class AtlasDetector(BaseDetector):
             for det_bboxes, det_scores, det_labels in bbox_list
         ]
         return bbox_results
-
 
     def aug_test(self, imgs, img_metas):
         pass
@@ -137,79 +102,32 @@ class AtlasDetector(BaseDetector):
         return torch.stack(projection)
 
 
-def get_origin(n_voxels, voxel_size, origin):
-    """
-    Args:
-        n_voxels (Tensor): sizes of the volume of shape (3,)
-        voxel_size (Tensor): sizes of one voxesl of shape (3,)
-        origin (Tensor): coordinates of the volume's center of shape (3,)
-    Returns:
-        Tensor: coordinates of the origin of shape (3,)
-    """
-    return origin - n_voxels / 2. * voxel_size
+@torch.no_grad()
+def get_points(n_voxels, voxel_size, origin):
+    points = torch.stack(torch.meshgrid([
+        torch.arange(n_voxels[0]),
+        torch.arange(n_voxels[1]),
+        torch.arange(n_voxels[2])
+    ]))
+    new_origin = origin - n_voxels / 2. * voxel_size
+    points = points * voxel_size.view(3, 1, 1, 1) + new_origin.view(3, 1, 1, 1)
+    return points
 
 
-# copy from https://github.com/magicleap/Atlas/blob/master/atlas/tsdf.py
-def coordinates(voxel_dim, device=torch.device('cuda')):
-    """ 3d meshgrid of given size.
-    Args:
-        voxel_dim: tuple of 3 ints (nx,ny,nz) specifying the size of the volume
-    Returns:
-        torch long tensor of size (3,nx*ny*nz)
-    """
-
-    nx, ny, nz = voxel_dim
-    x = torch.arange(nx, dtype=torch.long, device=device)
-    y = torch.arange(ny, dtype=torch.long, device=device)
-    z = torch.arange(nz, dtype=torch.long, device=device)
-    x, y, z = torch.meshgrid(x, y, z)
-    return torch.stack((x.flatten(), y.flatten(), z.flatten()))
-
-
-# copy from https://github.com/magicleap/Atlas/blob/master/atlas/model.py
-def backproject(voxel_dim, voxel_size, origin, projection, features):
-    """ Takes 2d features and fills them along rays in a 3d volume
-    This function implements eqs. 1,2 in https://arxiv.org/pdf/2003.10432.pdf
-    Each pixel in a feature image corresponds to a ray in 3d.
-    We fill all the voxels along the ray with that pixel's features.
-    Args:
-        voxel_dim: size of voxel volume to construct (nx,ny,nz)
-        voxel_size: metric size of each voxel (ex: .04m)
-        origin: origin of the voxel volume (xyz position of voxel (0,0,0))
-        projection: bx4x3 projection matrices (intrinsics@extrinsics)
-        features: bxcxhxw  2d feature tensor to be backprojected into 3d
-    Returns:
-        volume: b x c x nx x ny x nz 3d feature volume
-        valid:  b x 1 x nx x ny x nz volume.
-                Each voxel contains a 1 if it projects to a pixel
-                and 0 otherwise (not in view frustrum of the camera)
-    """
-
-    batch = features.size(0)
-    channels = features.size(1)
-    device = features.device
-    nx, ny, nz = voxel_dim
-
-    coords = coordinates(voxel_dim, device).unsqueeze(0).expand(batch, -1, -1)  # bx3xhwd
-    world = coords.type_as(projection) * voxel_size + origin.to(device).unsqueeze(2)
-    world = torch.cat((world, torch.ones_like(world[:, :1])), dim=1)
-
-    camera = torch.bmm(projection, world)
-    px = (camera[:, 0, :] / camera[:, 2, :]).round().type(torch.long)
-    py = (camera[:, 1, :] / camera[:, 2, :]).round().type(torch.long)
-    pz = camera[:, 2, :]
-
-    # voxels in view frustrum
-    height, width = features.size()[2:]
-    valid = (px >= 0) & (py >= 0) & (px < width) & (py < height) & (pz > 0)  # bxhwd
-
-    # put features in volume
-    volume = torch.zeros(batch, channels, nx * ny * nz, dtype=features.dtype,
-                         device=device)
-    for b in range(batch):
-        volume[b, :, valid[b]] = features[b, :, py[b, valid[b]], px[b, valid[b]]]
-
-    volume = volume.view(batch, channels, nx, ny, nz)
-    valid = valid.view(batch, 1, nx, ny, nz)
-
+# modify from https://github.com/magicleap/Atlas/blob/master/atlas/model.py
+def backproject(features, points, projection):
+    n_images, n_channels, height, width = features.shape
+    n_x_voxels, n_y_voxels, n_z_voxels = points.shape[-3:]
+    points = points.view(1, 3, -1).expand(n_images, 3, -1)
+    points = torch.cat((points, torch.ones_like(points[:, :1])), dim=1)
+    points_2d_3 = torch.bmm(projection, points)
+    x = (points_2d_3[:, 0] / points_2d_3[:, 2]).round().long()
+    y = (points_2d_3[:, 1] / points_2d_3[:, 2]).round().long()
+    z = points_2d_3[:, 2]
+    valid = (x >= 0) & (y >= 0) & (x < width) & (y < height) & (z > 0)
+    volume = torch.zeros((n_images, n_channels, points.shape[-1]), device=features.device)
+    for i in range(n_images):
+        volume[i, :, valid[i]] = features[i, :, y[i, valid[i]], x[i, valid[i]]]
+    volume = volume.view(n_images, n_channels, n_x_voxels, n_y_voxels, n_z_voxels)
+    valid = valid.view(n_images, 1, n_x_voxels, n_y_voxels, n_z_voxels)
     return volume, valid
