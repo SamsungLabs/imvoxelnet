75db94d05d47f17ca632cab5c3604903bb90ed58
Danila Rukhovich <danrukh@gmail.com>
2021-04-07 14:27:00 +0300

    mode voxels on kitti

--- mmdet3d/models/necks/__init__.py
+++ mmdet3d/models/necks/__init__.py
@@ -1,5 +1,5 @@
 from mmdet.models.necks.fpn import FPN
 from .second_fpn import SECONDFPN
-from .atlas import AtlasNeck, AtlasNeckV2
+from .atlas import AtlasNeck, AtlasNeckV2, AtlasNeckV3
 
 __all__ = ['FPN', 'SECONDFPN', 'AtlasNeck', 'AtlasNeckV2']
