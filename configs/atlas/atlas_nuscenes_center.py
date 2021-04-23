45fda3ce1acdf7ee64e9ffd46608527d157a86ff
Danila Rukhovich <danrukh@gmail.com>
2021-04-23 23:43:05 +0300

    try center head for nuscenes; add visualization

---
+++ configs/atlas/atlas_nuscenes_center.py
@@ -0,0 +1,171 @@
+model = dict(
+    type='AtlasDetector',
+    pretrained='torchvision://resnet50',
+    backbone=dict(
+        type='ResNet',
+        depth=50,
+        num_stages=4,
+        out_indices=(0, 1, 2, 3),
+        frozen_stages=1,
+        norm_cfg=dict(type='BN', requires_grad=False),
+        norm_eval=True,
+        style='pytorch'),
+    neck=dict(
+        type='FPN',
+        in_channels=[256, 512, 1024, 2048],
+        out_channels=64,
+        num_outs=4),
+    neck_3d=dict(
+        type='KittiAtlasNeck',
+        in_channels=64,
+        out_channels=256),
+    bbox_head=dict(
+        type='CenterHead',
+        in_channels=256,  # todo: 512 ?
+        tasks=[
+            dict(num_class=1, class_names=['car'])
+        ],
+        common_heads=dict(
+            reg=(2, 2), height=(1, 2), dim=(3, 2), rot=(2, 2)),
+        share_conv_channel=64,
+        bbox_coder=dict(
+            type='CenterPointBBoxCoder',
+            pc_range=[-51.2, -51.2],
+            post_center_range=[-51.2, -51.2, -3.0, 51.2, 51.2, 1.0],
+            max_num=500,
+            score_threshold=0.1,
+            out_size_factor=8,
+            voxel_size=[.1, .1],
+            code_size=7),
+        seperate_head=dict(
+            type='SeparateHead', init_bias=-2.19, final_kernel=3),
+        loss_cls=dict(type='GaussianFocalLoss', reduction='mean'),
+        loss_bbox=dict(type='L1Loss', reduction='mean', loss_weight=0.25),
+        norm_bbox=True),
+    n_voxels=(128, 128, 8),
+    voxel_size=(.8, .8, .5))
+# model training and testing settings
+train_cfg = dict(
+    grid_size=[1024, 1024, 40],
+    voxel_size=[.1, .1, .2],  # todo: Why .2 ? Unused?
+    out_size_factor=8,
+    dense_reg=1,
+    gaussian_overlap=0.1,
+    max_objs=500,
+    min_radius=2,
+    code_weights=[1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+    point_cloud_range=[-51.2, -51.2, -3.0, 51.2, 51.2, 1.0])
+test_cfg = dict(
+    post_center_limit_range=[-51.2, -51.2, -3.0, 51.2, 51.2, 1.0],
+    max_per_img=500,
+    max_pool_nms=False,
+    min_radius=[4, 12, 10, 1, 0.85, 0.175],
+    score_threshold=0.1,
+    out_size_factor=8,
+    voxel_size=[.1, .1],
+    nms_type='rotate',
+    pre_max_size=1000,
+    post_max_size=83,
+    nms_thr=0.2,
+    pc_range=[-51.2, -51.2])
+img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+
+dataset_type = 'NuScenesMultiViewDatasetV2'
+data_root = 'data/nuscenes/'
+point_cloud_range = [-51.2, -51.2, -3, 51.2, 51.2, 1]
+class_names = [
+    'car', 'truck', 'trailer', 'bus', 'construction_vehicle', 'bicycle',
+    'motorcycle', 'pedestrian', 'traffic_cone', 'barrier'
+]
+input_modality = dict(
+    use_lidar=False,
+    use_camera=True,
+    use_radar=False,
+    use_map=False,
+    use_external=False)
+
+train_pipeline = [
+    dict(type='LoadAnnotations3D'),
+    dict(
+        type='ScanNetMultiViewPipeline',
+        n_images=6,
+        transforms=[
+            dict(type='LoadImageFromFile'),
+            dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32)]),
+    dict(type='ObjectRangeFilter', point_cloud_range=point_cloud_range),
+    dict(type='KittiSetOrigin', point_cloud_range=point_cloud_range),
+    dict(type='DefaultFormatBundle3D', class_names=class_names),
+    dict(type='Collect3D', keys=['img', 'gt_bboxes_3d', 'gt_labels_3d'])]
+test_pipeline = [
+    dict(
+        type='ScanNetMultiViewPipeline',
+        n_images=6,
+        transforms=[
+            dict(type='LoadImageFromFile'),
+            dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32)]),
+    dict(type='KittiSetOrigin', point_cloud_range=point_cloud_range),
+    dict(type='DefaultFormatBundle3D', class_names=class_names, with_label=False),
+    dict(type='Collect3D', keys=['img'])]
+
+data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=2,
+    train=dict(
+        type='RepeatDataset',
+        times=1,
+        dataset=dict(
+            type=dataset_type,
+            data_root=data_root,
+            ann_file=data_root + 'nuscenes_infos_train.pkl',
+            pipeline=train_pipeline,
+            classes=class_names,
+            modality=input_modality,
+            test_mode=False,
+            box_type_3d='LiDAR')),
+    val=dict(
+        type=dataset_type,
+        data_root=data_root,
+        ann_file=data_root + 'nuscenes_infos_val.pkl',
+        pipeline=test_pipeline,
+        classes=class_names,
+        modality=input_modality,
+        test_mode=True,
+        box_type_3d='LiDAR'),
+    test=dict(
+        type=dataset_type,
+        data_root=data_root,
+        ann_file=data_root + 'nuscenes_infos_val.pkl',
+        pipeline=test_pipeline,
+        classes=class_names,
+        modality=input_modality,
+        test_mode=True,
+        box_type_3d='LiDAR'))
+
+optimizer = dict(
+    type='AdamW',
+    lr=0.0001,
+    weight_decay=0.0001,
+    paramwise_cfg=dict(
+        custom_keys={'backbone': dict(lr_mult=0.1, decay_mult=1.0)}))
+optimizer_config = dict(grad_clip=dict(max_norm=35., norm_type=2))
+lr_config = dict(policy='step', step=[8, 11])
+total_epochs = 12
+
+checkpoint_config = dict(interval=1)
+log_config = dict(
+    interval=50,
+    hooks=[
+        dict(type='TextLoggerHook'),
+        dict(type='TensorboardLoggerHook')
+    ])
+evaluation = dict(interval=100)
+dist_params = dict(backend='nccl')
+find_unused_parameters = True  # todo: fix number of FPN outputs
+log_level = 'INFO'
+load_from = None
+resume_from = None
+workflow = [('train', 1)]
