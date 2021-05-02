e0c003e023957649ef376933f1553fb652bd385b
Danila Rukhovich <danrukh@gmail.com>
2021-05-02 17:54:00 +0300

    the 10 class nuscenes with second and fpn

--- mmdet3d/models/necks/__init__.py
+++ mmdet3d/models/necks/__init__.py
@@ -1,5 +1,5 @@
 from mmdet.models.necks.fpn import FPN
 from .second_fpn import SECONDFPN
-from .atlas import AtlasNeck, KittiAtlasNeck, KittiSyncAtlasNeck
+from .atlas import AtlasNeck, KittiAtlasNeck, KittiSyncAtlasNeck, NuScenesAtlasNeck
 
 __all__ = ['FPN', 'SECONDFPN', 'AtlasNeck', 'KittiAtlasNeck']
