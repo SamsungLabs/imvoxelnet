cacd12cc3b271638ac45726f82e5a9c4e203b285
Danila Rukhovich <danrukh@gmail.com>
2021-05-03 18:11:31 +0300

    try 1x1x1 conv for kitti

--- mmdet3d/models/necks/__init__.py
+++ mmdet3d/models/necks/__init__.py
@@ -1,5 +1,5 @@
 from mmdet.models.necks.fpn import FPN
 from .second_fpn import SECONDFPN
-from .atlas import AtlasNeck, KittiAtlasNeck, NuScenesAtlasNeck
+from .atlas import AtlasNeck, KittiAtlasNeck, NuScenesAtlasNeck, KittiAtlasNeckV2
 
 __all__ = ['FPN', 'SECONDFPN', 'AtlasNeck', 'KittiAtlasNeck']
