50bd30c9e2d638a129e55c213b4e312531d6c77f
Danila Rukhovich <danrukh@gmail.com>
2021-05-05 15:17:15 +0300

    update kitti default config with 12 z voxels

--- mmdet3d/models/necks/__init__.py
+++ mmdet3d/models/necks/__init__.py
@@ -1,6 +1,6 @@
 from mmdet.models.necks.fpn import FPN
 from .second_fpn import SECONDFPN
-from .atlas import AtlasNeck, KittiAtlasNeck, NuScenesAtlasNeck, KittiAtlasNeckV2, KittiAtlasNeckV3
+from .atlas import AtlasNeck, KittiAtlasNeck, NuScenesAtlasNeck, KittiAtlasNeckV3
 from .atlas_v2 import NuScenesAtlasNeckV2
 
 __all__ = ['FPN', 'SECONDFPN', 'AtlasNeck', 'KittiAtlasNeck']
--- mmdet3d/models/necks/atlas.py
+++ mmdet3d/models/necks/atlas.py
@@ -60,36 +60,6 @@ class KittiAtlasNeck(nn.Module):
 
 
 @NECKS.register_module()
-class KittiAtlasNeckV2(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super().__init__()
-        self.model = nn.Sequential(
-            BasicBlock3d(in_channels, in_channels),
-            self._get_conv(in_channels, in_channels * 2),
-            BasicBlock3d(in_channels * 2, in_channels * 2),
-            self._get_conv(in_channels * 2, in_channels * 4),
-            BasicBlock3d(in_channels * 4, in_channels * 4),
-            self._get_conv(in_channels * 4, out_channels)
-        )
-
-    @staticmethod
-    def _get_conv(in_channels, out_channels):
-        return nn.Sequential(
-            nn.Conv3d(in_channels, out_channels, 1, stride=(1, 1, 2), padding=0),
-            nn.BatchNorm3d(out_channels),
-            nn.ReLU(inplace=True)
-        )
-
-    @auto_fp16()
-    def forward(self, x):
-        x = self.model.forward(x)
-        return [x[..., 0].transpose(-1, -2)]
-
-    def init_weights(self):
-        pass
-
-
-@NECKS.register_module()
 class KittiAtlasNeckV3(nn.Module):
     def __init__(self, in_channels, out_channels):
         super().__init__()
