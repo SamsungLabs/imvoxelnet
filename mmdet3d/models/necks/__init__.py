f6f4a9e62b9f83a5bfb4c1d7c318053888411bf1
Danila Rukhovich <danrukh@gmail.com>
2021-05-05 17:50:45 +0300

    try 400 voxels for nuscenes full

--- mmdet3d/models/necks/__init__.py
+++ mmdet3d/models/necks/__init__.py
@@ -1,6 +1,6 @@
 from mmdet.models.necks.fpn import FPN
 from .second_fpn import SECONDFPN
 from .atlas import AtlasNeck, KittiAtlasNeck, KittiAtlasNeckV3, NuScenesAtlasNeckV3
-from .atlas_v2 import NuScenesAtlasNeckV2
+from .atlas_v2 import NuScenesAtlasNeckV2, NuScenesFullAtlasNeck
 
 __all__ = ['FPN', 'SECONDFPN', 'AtlasNeck', 'KittiAtlasNeck']
