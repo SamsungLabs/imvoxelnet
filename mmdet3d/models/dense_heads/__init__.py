419f593c1153dc48da56b3e2a2647e67837b6e52
Danila Rukhovich <danrukh@gmail.com>
2021-05-06 15:05:24 +0300

    try atan2 for total sunrgbd

--- mmdet3d/models/dense_heads/__init__.py
+++ mmdet3d/models/dense_heads/__init__.py
@@ -1,5 +1,4 @@
 from .anchor3d_head import Anchor3DHead
-from .anchor3d_head_v2 import Anchor3DHeadV2
 from .base_conv_bbox_head import BaseConvBboxHead
 from .centerpoint_head import CenterHead
 from .free_anchor3d_head import FreeAnchor3DHead
@@ -8,14 +7,13 @@ from .shape_aware_head import ShapeAwareHead
 from .ssd_3d_head import SSD3DHead
 from .vote_head import VoteHead
 from .fcos_3d_head import FCOS3DHead
-from .fcos_3d_head_v2 import FCOS3DHeadV2
 from .transformer_3d_head import Transformer3DHead
-from .voxel_fcos_head import SUNRGBDVoxelFCOSHead, ScanNetVoxelFCOSHead
+from .voxel_fcos_head import SUNRGBDVoxelFCOSHead, ScanNetVoxelFCOSHead, SUNRGBDVoxelFCOSHeadV2
 from .layout_head import LayoutHead
 
 __all__ = [
     'Anchor3DHead', 'FreeAnchor3DHead', 'PartA2RPNHead', 'VoteHead',
     'SSD3DHead', 'BaseConvBboxHead', 'CenterHead', 'ShapeAwareHead',
-    'FCOS3DHead', 'FCOS3DHeadV2', 'Transformer3DHead',
+    'FCOS3DHead', 'Transformer3DHead',
     'SUNRGBDVoxelFCOSHead', 'ScanNetVoxelFCOSHead'
 ]
