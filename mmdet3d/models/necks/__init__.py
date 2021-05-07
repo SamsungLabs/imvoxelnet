42baf379b5fe777b2fbb348930804f7d64fa1f04
Danila Rukhovich <danrukh@gmail.com>
2021-05-07 18:06:05 +0300

    remove nuscenes_full

--- mmdet3d/models/necks/__init__.py
+++ mmdet3d/models/necks/__init__.py
@@ -1,6 +1,5 @@
 from mmdet.models.necks.fpn import FPN
 from .second_fpn import SECONDFPN
 from .atlas import AtlasNeck, KittiAtlasNeck, KittiAtlasNeckV3, KittiAtlasNeckV4, NuScenesAtlasNeckV3, NuScenesAtlasNeckV4
-from .atlas_v2 import NuScenesAtlasNeckV2, NuScenesFullAtlasNeck
 
 __all__ = ['FPN', 'SECONDFPN', 'AtlasNeck', 'KittiAtlasNeck']
