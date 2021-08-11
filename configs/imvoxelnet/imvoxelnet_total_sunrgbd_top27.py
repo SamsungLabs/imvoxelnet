8270bf09ebc1671d26f2124f3ac27a2fda8b53d9
Danila Rukhovich <danrukh@gmail.com>
2021-08-11 15:33:36 +0300

    add resnet 18 and 34 experiments

--- configs/imvoxelnet/imvoxelnet_total_sunrgbd_top27.py
+++ configs/imvoxelnet/imvoxelnet_total_sunrgbd_top27.py
@@ -44,7 +44,7 @@ test_cfg = dict(
     nms_pre=1000,
     nms_thr=.15,
     use_rotate_nms=True,
-    score_thr=.05)
+    score_thr=.0)
 img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 
 dataset_type = 'SunRgbdTotalMultiViewDataset'
